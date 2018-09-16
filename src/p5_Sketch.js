08f18786f3af8bb87d3f5fc14835fee43f4842b0
cccristanchoc <cccristanchoc@unal.edu.co>
2018-09-15 23:08:15 -0500

    Multiples canvas p5-js

--- src/p5_Sketch.js
+++ src/p5_Sketch.js
@@ -1,15 +1,55 @@
-function setup() {
-  var canvas = createCanvas(747/2, 747/2);
+// function setup() {
+//   var cnvs = createCanvas(480, 260);
+//
+//   // Move the canvas so it’s inside our <div id="sketch-holder">.
+//   cnvs.parent('skmo');
+// }
+//
+// function draw() {
+//   if (mouseIsPressed) {
+//     fill(0);
+//   } else {
+//     fill(255);
+//   }
+//   ellipse(mouseX, mouseY, 80, 80);
+// }
 
-  // Move the canvas so it’s inside our <div id="sketch-holder">.
-  canvas.parent('sketch');
-}
+// save this file as sketch.js
+// Sketch One
+var s = function( p ) { // p could be any variable name
+  p.setup = function() {
+    p.createCanvas(400, 200);
+  };
 
-function draw() {
-  if (mouseIsPressed) {
-    fill(0);
-  } else {
-    fill(255);
-  }
-  ellipse(mouseX, mouseY, 80, 80);
-}
+  p.draw = function() {
+    if (p.mouseIsPressed) {
+      p.fill(0);
+    } else {
+      p.fill(255);
+    }
+    p.ellipse(p.mouseX, p.mouseY, 80, 80);
+  };
+};
+var myp5 = new p5(s, 'skmo');
+
+// Sketch Two
+var t = function( p ) {
+  var x = 100.0;
+  var y = 100;
+  var speed = 2.5;
+  p.setup = function() {
+    p.createCanvas(400, 200);
+  };
+
+  p.draw = function() {
+    p.background(100);
+    p.fill(1);
+    x += speed;
+    if(x > p.width){
+      x = 0;
+    }
+    p.ellipse(x,y,50,50);
+
+  };
+};
+var myp5 = new p5(t, 'sk-test');
--- src/test.js
+++ src/test.js
@@ -1,13 +1,32 @@
-var img;
-function preload() {
-  var canvas = createCanvas(245, 489);
-  img = loadImage('images/face-up.png');
-
-  // Move the canvas so it’s inside our <div id="sketch-holder">.
-  canvas.parent('sketch');
-}
+var distances = [];
+var maxDistance;
+var spacer;
 
 function setup() {
-  image(img, 0, 0);
-  image(img, 0, height/2, img.width/2, img.height/2);
+  var cn = createCanvas(480, 260);
+  cn.parent('sk-test');
+  maxDistance = dist(width/2, height/2, width, height);
+  for (var x = 0; x < width; x++) {
+    distances[x] = []; // Crear un arreglo anidado
+    for (var y = 0; y < height; y++) {
+      var distance = dist(width/2, height/2, x, y);
+      distances[x][y] = distance/maxDistance * 255;
+    }
+  }
+  spacer = 10;
+  noLoop();  // Correr draw() una vez y luego parar
+}
+
+function draw() {
+  background(0);
+  // Este loop anidado recorre los valores en los arreglos basado
+  // en la variable spacer, así que hay más valores en el arreglo
+  // que los que dibujamos. Cambia el valor de la variable spacer
+  // para cambiar la densidad de los puntos.
+  for (var x = 0; x < width; x += spacer) {
+    for (var y = 0; y < height; y += spacer) {
+      stroke(distances[x][y]);
+      point(x + spacer/2, y + spacer/2);
+    }
+  }
 }
